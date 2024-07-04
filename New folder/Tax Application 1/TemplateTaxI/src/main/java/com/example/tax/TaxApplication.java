b1ad1226919e5480f4f6d4a05fc1daa7ef0f0ae3
saumyasingh <saumya.singh@nagarro.com>
2024-07-04 15:51:13 +0530

    Tax Application Completed

--- New folder/Tax Application 1/TemplateTaxI/src/main/java/com/example/tax/TaxApplication.java
+++ New folder/Tax Application 1/TemplateTaxI/src/main/java/com/example/tax/TaxApplication.java
@@ -10,15 +10,12 @@ public class TaxApplication {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Scanner sc = new Scanner(System.in);
-		int userChoice=0;
-		String taxChoice="";
-		int amount;
-		char ch;
-		System.out.println("\nWelcome to the Tax Payment Application");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Welcome to the Tax Payment Application");
 		while(true) {
-			System.out.println("\nPlease select which tax you want to do?\n1.Calculate Income Tax\n2.Calculate Property Tax\n3.Exit");
-			userChoice=sc.nextInt();
+			System.out.println("Please select which tax you want to pay: \n1. Income \n2. Property\n3. Exit");
+			int userChoice = scanner.nextInt();
+			String taxChoice = "";
 			switch(userChoice) {
 				case 1:
 					taxChoice="incomeTax";				
@@ -28,27 +25,13 @@ public class TaxApplication {
 				break;
 				case 3:
 					System.out.println("Now exiting");
-					System.exit(0);
-				break;
+					return;
 				
 				default:
 					System.out.println("Invalid Choice.. Renter");
 			}	
 			Tax tax=(Tax)context.getBean(taxChoice);
 			System.out.println(tax.getTaxType());
-			if(tax.isTaxPayed()) {
-				System.out.println("Tax is already paid!!!");
-			}else {
-				System.out.println("Enter taxable amount.");
-				amount = sc.nextInt();
-				tax.setTaxableAmount(amount);
-				tax.calculateTaxAmount();
-				System.out.println("Your tax amount is:"+ tax.getTaxAmount());
-				System.out.println("Do you want to pay Tax?(Y/N)");
-				ch=sc.next().charAt(0);
-				if(ch=='y'||ch=='Y') 
-				tax.payTax();
-			}
 		}
 		
 	}
