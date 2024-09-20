b4d7865eb987a0083e1f14aa218d38f12d6df79d
saumyasingh <saumya.singh@nagarro.com>
2024-09-20 17:56:09 +0530

    Movie Seat Booking Portal

---
+++ Assignments/Movie Seat Booking Portal/Seat Booking Service/seat-booking-service/src/main/java/com/example/seat_booking_service/SeatBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.seat_booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatBookingServiceApplication.class, args);
+	}
+
+}
